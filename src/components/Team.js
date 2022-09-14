4241a67a79091c25c0523a175263ce7b9f1a23fc
hayatul-islam <hayatulemon340@gmail.com>
2022-09-14 20:21:35 +0600

    animation complete

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,20 +1,34 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 export default function Team({ team }) {
   const { id, img, name, job } = team || {};
   return (
     <>
-      <div
-        className={`space-y-3 ${id % 2 === 0 && "sm:pt-16 sm:ml-[-50px]"} pb-6`}
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.8,
+          delay: 0.5,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
       >
-        <div>
-          <img className="w-[300px] h-[280px]" src={img} alt={name} />
+        <div
+          className={`space-y-3 ${
+            id % 2 === 0 && "sm:pt-16 sm:ml-[-50px]"
+          } pb-6`}
+        >
+          <div>
+            <img className="w-[300px] h-[280px]" src={img} alt={name} />
+          </div>
+          <div>
+            <h4 className="text-lg font-medium">{name}</h4>
+            <p className="text-sm">{job}</p>
+          </div>
         </div>
-        <div>
-          <h4 className="text-lg font-medium">{name}</h4>
-          <p className="text-sm">{job}</p>
-        </div>
-      </div>
+      </motion.div>
     </>
   );
 }
